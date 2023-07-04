3c1399a36fc2332daee1bd8d9bf4f06c616ce69d
Jack Leow <jackgene@gmail.com>
2023-07-04 14:06:10 -0700

    Added Elm exception-safety code snippets.

---
+++ strongly-typed-elm/exception_safety/safe_invalid/ExceptionSafety.elm
@@ -0,0 +1,9 @@
+module ExceptionSafety exposing (..)
+import Json.Decode
+
+decodedInt : Result Json.Decode.Error Int
+decodedInt = Json.Decode.decodeString Json.Decode.int "42"
+
+squared : Int
+squared = case decodedInt of
+  Ok num -> num * num
